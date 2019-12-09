79f6087ae477e3f3a091884df19a9e38224fc676
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-09 17:22:04 +0500

    Create century.py

---
+++ centuryВек/century.py
@@ -0,0 +1,4 @@
+v = ['I', 'II', 'III', 'IV', '  V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','IXX','XX','XXI']
+g = int(input())
+century = g//100
+print(v[century]) 
